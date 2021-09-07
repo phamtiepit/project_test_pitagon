5a122f373c092465f04f7199a43f72915a2df943
tieptute2k <tieptute2k@gmail.com>
2021-09-07 17:33:58 +0700

    update api

--- be-project-test/src/main/java/com/tieppv/beprojecttest/controller/MainController.java
+++ be-project-test/src/main/java/com/tieppv/beprojecttest/controller/MainController.java
@@ -1,24 +1,30 @@
 package com.tieppv.beprojecttest.controller;
 
 
-import java.util.Base64;
 import java.util.Map;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
+import com.tieppv.beprojecttest.service.ConvertService;
+
 
 @RestController
 @CrossOrigin(origins = "http://localhost:4200")
 @RequestMapping(path = "/api")
 public class MainController {
 
+	@Autowired
+	ConvertService convertService;
+	
+	
 	@PostMapping(path = "/convert")
 	@ResponseBody
 	public String convertToBase64(@RequestBody Map<String, String> request) {
@@ -26,24 +32,27 @@ public class MainController {
 		String inputString = request.get("inputString").replace("\"", "");
 		String convertType =  request.get("convertType");
 			
-		//System.out.print(inputString);
-		if(convertType.equals("stringtobase64")) {
-			result = encodeString(inputString);
-		}else if(convertType.equals("base64tostring")){
-			result = decodeString(inputString);
+		switch(convertType) {
+		  case "stringtobase64":
+			  result = convertService.encodeString(inputString);
+		    break;
+		    
+		  case "base64tostring":
+			  result = convertService.decodeString(inputString);
+		    break;
 		}
-		
+		//System.out.print(inputString);
 		return result;
 	}
 	
-	private String encodeString(String input) {
-		String convertedString = Base64.getEncoder().encodeToString(input.getBytes());
-		Gson gson= new GsonBuilder().create();
-		return gson.toJson(convertedString);
-	}
-	private String decodeString(String input) {
-		byte[] decodedBytes = Base64.getDecoder().decode(input);
-		Gson gson= new GsonBuilder().create();
-		return gson.toJson(new String(decodedBytes));
+	
+	
+	@PostMapping(path = "/convert/image")
+	@ResponseBody
+	public String encodeImage(@RequestParam("image") MultipartFile image) {
+		
+		System.out.print(image);
+		
+		return null;
 	}
 }
---
+++ be-project-test/src/main/java/com/tieppv/beprojecttest/service/ConvertService.java
@@ -0,0 +1,32 @@
+package com.tieppv.beprojecttest.service;
+
+import java.util.Base64;
+
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+@Service
+public class ConvertService {
+	
+	public String encodeString(String input) {
+		String convertedString = Base64.getEncoder().encodeToString(input.getBytes());
+		Gson gson= new GsonBuilder().create();
+		return gson.toJson(convertedString);
+	}
+	public String decodeString(String input) {
+		byte[] decodedBytes = Base64.getDecoder().decode(input);
+		Gson gson= new GsonBuilder().create();
+		return gson.toJson(new String(decodedBytes));
+	}
+	
+	public String encodeImageToBase64(MultipartFile file) {
+		String base64Image = "";
+		
+			
+		
+		return base64Image;
+	}
+}
