94959fd476ad0129c4fefc16d4246c074b5e5877
tieptute2k <tieptute2k@gmail.com>
2021-09-07 17:31:50 +0700

    update api

--- fe-project-test/src/app/convert/convert.component.ts
+++ fe-project-test/src/app/convert/convert.component.ts
@@ -4,38 +4,66 @@ import { HttpClient } from '@angular/common/http';
 @Component({
   selector: 'app-convert',
   templateUrl: './convert.component.html',
-  styleUrls: ['./convert.component.css']
+  styleUrls: ['./convert.component.css'],
 })
 export class ConvertComponent implements OnInit {
-
   isShow = false;
-  result: string =''
+  result: string = '';
+  imageSrc: string = '';
+  selectedFile!: File;
 
+  constructor(private http: HttpClient) { }
 
-  constructor(private http:HttpClient) {}
-
-  ngOnInit(): void {
+  ngOnInit(): void { }
+  onSubmit(formConvert: any) {
+    //console.log(formConvert);
+    if (formConvert.value.convertType !== 'imagetobase64') {
+      this.sendToServer(formConvert);
+    }else{
+      this.uploadImage();
+    }
   }
-  onSubmit(formConvert:any){
-    console.log(formConvert);
-    //this.sendToServer(formConvert);
-  }
 
-  sendToServer(formConvert:any){
-    const url = "http://localhost:8080/api/convert";
-    this.http.post(url, formConvert.value)
-    .subscribe(response => {console.log(response)
-      this.result =  JSON.stringify(response);
-
+  sendToServer(formConvert: any) {
+    const url = 'http://localhost:8080/api/convert';
+    this.http.post(url, formConvert.value).subscribe((response) => {
+      console.log(response);
+      this.result = JSON.stringify(response);
     });
   }
-  onChange(event:any){
-    //console.log(event);
-    if(event === 'imagetobase64' || event === 'base64toimage'){
+  onSelectChange(event: any) {
+    if (event === 'imagetobase64') {
       this.isShow = true;
-    }else{
+    } else {
       this.isShow = false;
     }
   }
 
+  onFileChange(event: any) {
+    //console.log(event);
+    const reader = new FileReader();
+
+    if (event.target.files && event.target.files.length) {
+      const [file] = event.target.files;
+      this.selectedFile = file;
+      reader.onload = (event: any) => {
+        this.imageSrc = event.target.result;
+      }
+      reader.readAsDataURL(file);
+    }
+  }
+  uploadImage() {
+    console.log(this.selectedFile);
+    const uploadImageData = new FormData();
+    uploadImageData.append('imageFile', this.selectedFile, this.selectedFile.name);
+    this.http.post('http://localhost:8080/api/convert/image', uploadImageData, { observe: 'response' })
+      .subscribe((response:any) => {
+        if (response.status === 200) {
+          console.log('Image uploaded successfully');
+        } else {
+          console.log('Image uploaded error');
+        }
+      }
+      );
+  }
 }
